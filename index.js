26b05b1702fa4050434b2fcbe6724a98ffa9019b
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-04-20 13:22:04 -0400

    write function extract_photos and add notes

--- index.js
+++ index.js
@@ -28,8 +28,35 @@ async function extract_name() {
 	const page = await browser.newPage();
 	await page.goto('https://learnwebcode.github.io/practice-requests/');
 
-	const names = ['red', 'blue', 'yellow'];
+	// use evaluate to scan the webpage
+	const names = await page.evaluate(() => {
+		// write any clientside browser javascript code
+		// scope is browser/chrome land NOT node.js land
+		return Array.from(document.querySelectorAll('.info strong')).map(
+			(x) => x.textContent
+		);
+	});
 	await fs.writeFile('names.txt', names.join('\r\n'));
+
+	await browser.close();
+}
+
+async function extract_photos() {
+	// Code Here
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('https://learnwebcode.github.io/practice-requests/');
+
+	// $$eval() = select multiple elements
+	const photos = await page.$$eval('img', (imgs) => {
+		return imgs.map((x) => x.src);
+	});
+
+	for (const photo of photos) {
+		const imagePage = await page.goto(photo);
+		// split the url at the last / and use the last poriton which is the image name
+		await fs.writeFile(photo.split('/').pop(), await imagePage.buffer());
+	}
 
 	await browser.close();
 }
@@ -38,4 +65,6 @@ async function extract_name() {
 
 // screenshot_fullpage();
 
-extract_name();
+// extract_name();
+
+extract_photos();
