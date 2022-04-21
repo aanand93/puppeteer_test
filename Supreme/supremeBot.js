5d990d347d88e12d23b4e2e3b4cbc8ae54512bdc
aanand93 <77586218+aanand93@users.noreply.github.com>
2022-04-21 12:10:31 -0400

    Update automation on supreme bot

--- Supreme/supremeBot.js
+++ Supreme/supremeBot.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs/promises');
 
+// Run the function below and a .txt file will be created labeled with the date you run the scraper on. Inside the file all the urls for the new items will be listed.
+
 async function extractHref() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
@@ -11,13 +13,13 @@ async function extractHref() {
 		return links.map((x) => x.href);
 	});
 
+	// Create todays date for filename
 	let today = new Date();
 	let dd = String(today.getDate()).padStart(2, '0');
 	let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 	let yyyy = today.getFullYear();
 
 	today = mm + '_' + dd + '_' + yyyy;
-	// document.write(today);
 
 	for (const href of hrefs) {
 		const hrefPage = await page.goto(href);
@@ -29,3 +31,10 @@ async function extractHref() {
 }
 
 extractHref();
+
+/* ------------------------------------------------*/
+/*-------------------Automation--------------------*/
+/* ------------------------------------------------*/
+
+// Run function Once A Day
+// setInterval(extractHref, 86400000);
